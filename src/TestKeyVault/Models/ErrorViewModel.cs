fda319a6c3262b557782599660bd0b5d0e0aaa38
Jeremy Lindsay <jeremy.lindsay@outlook.com>
2018-03-11 12:59:23 +0000

    Initial commit

---
+++ src/TestKeyVault/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestKeyVault.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
